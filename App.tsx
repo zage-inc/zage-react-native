04f8b25e726e81d7f2e22507e8debf81b7399dda
Michael Sun <michael@zage.app>
2022-03-17 18:52:58 -0700

    ZageRN testing app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { Component } from 'react';
+import { StyleSheet, Button, View } from 'react-native';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+import { Zage } from '@zage/zage-react-native'
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+export default class App extends Component<{}, {
+  showZage: boolean;
+}>{
+  // const isLoadingComplete = useCachedResources();
+
+  // const [showZage, setShowZage] = useState<boolean>(false);
+  // if (!isLoadingComplete) {
+  //   return null;
+  // } else {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showZage: false
+    } 
   }
+  setShowZage(b: boolean) {
+    this.setState({
+      showZage: b
+    })
+  }
+  render () {
+    const { showZage } = this.state
+    return (
+      <View style={styles.container}>
+        {/* Button that opens Zage payment flow */}
+        <Button title="Pay with Zage" onPress={() => this.setShowZage(true)}/>
+          {/* Zage component with necessary props */} 
+          <Zage
+            publicKey="sandbox_key_60jp0v85tbpwev8p"
+            paymentToken="tk_5114a252-a28e-4f81-a00b-d18bd1d452b0"
+            onComplete={(res: any) => {
+              console.log(res)
+            }}
+            onExit={() => {
+              console.log('Exited payment')
+            }}
+            showZage={showZage} //
+            setShowZage={this.setShowZage}
+          />
+      </View> 
+    )
+  }
+  // }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+});
+
