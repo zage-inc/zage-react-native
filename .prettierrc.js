3f5a85cd4d34dd32f57657fb27105044d0d47bb2
Cole Bransford <cole@zage.app>
2022-03-30 10:00:01 -0700

    improved use effect, better peer Deps, prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 100,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
